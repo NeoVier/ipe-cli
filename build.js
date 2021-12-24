bad967876e8588dd78dd7e1c613e62f278079c2c
Henrique Buss <henrique.buss@hotmail.com>
2021-12-23 21:21:40 -0300

    Fix build and execute scripts. Use production environment for build command

--- build.js
+++ build.js
@@ -6,5 +6,5 @@ esbuild.build({
     bundle: true,
     outdir: 'dist',
     watch: process.argv.includes('--watch'),
-    plugins: [ElmPlugin()]
+    plugins: [ElmPlugin({ optimize: process.env.NODE_ENV === 'production' })]
 }).catch(_e => process.exit(1))
